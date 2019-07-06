b7c6d3f181161719c25d3a4bbca527067c342fb8
HuyenTran112 <huyentranbui112@gmail.com>
2019-07-06 15:30:21 +0700

    update user+ product

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\User;
+use App\bill;
 use App\Http\Requests\LoginRequest;
 
 
@@ -15,7 +16,8 @@ class UserController extends Controller
 {
 	//Hiện thị danh sách user admin
     public function getList(){
-        $listItem = DB::table('users')->where('level','!=','0')->get();
+		$id=Auth::user()->id;
+        $listItem = DB::table('users')->where('level','!=','0')->where('id','!=',$id)->get();
         return view('admin.user.list',compact('listItem'));
     }
 	//Thêm user admin
@@ -28,13 +30,13 @@ class UserController extends Controller
             [
                 'txtEmail'=>'email|unique:users,email',
                 'txtPass'=>'min:6|max:20',
-                'txtRepass'=>'same:password',
+                'txtRePass'=>'same:txtPass',
                 'txtName'=>'min:3'
             ],
             [
                 'txtEmail.email'=>'Không đúng định dạng email',
                 'txtEmail.unique'=>'Email đã có người sử dụng',
-                'txtRepass.same'=>'Mật khẩu không giống nhau',
+                'txtRePass.same'=>'Mật khẩu không giống nhau',
                 'txtPass.min'=>'Mật khẩu ít nhất 6 kí tự',
                 'txtName.min'=>'Tên hiện thị ít nhất 3 ký tự'
             ]);
@@ -43,7 +45,7 @@ class UserController extends Controller
         $username->password = Hash::make($req->txtPass);
         $username->level = $req->rdoLevel;
         $username->tenhienthi=$req->txtTenHienThi;
-		DB::table('users')->insert(['email'=>$req->txtEmail,'password'=>Hash::make($req->txtPass),'level'=>$req->rdoLevel,'tenhienthi'=>$req->TenHienThi]);
+		$kt=DB::table('users')->insert(['email'=>$req->txtEmail,'password'=>Hash::make($req->txtPass),'level'=>$req->rdoLevel,'tenhienthi'=>$req->TenHienThi]);
         return redirect()->route('admin.user.list')->with(['flash_level'=>'success','flash_message'=>'Thêm user admin thành công']);
     }
 	//Xóa user
@@ -51,7 +53,7 @@ class UserController extends Controller
         $user_current_login = Auth::user()->id;
 		$level_current_login = Auth::user()->level;
         $username = DB::table('users')->where('id',$id)->first();
-        if(($user_current_login == $id) || ($level_current_login !=1 && $username->level ==1)){
+        if(($user_current_login == $id) || ($level_current_login !=1 )|| ($username->level ==1)){
             return redirect()->route('admin.user.list')->with(['flash_level'=>'danger','flash_message'=>'Bạn không thể xóa user này']);
         }
         else{
@@ -59,7 +61,7 @@ class UserController extends Controller
     		return redirect()->route('admin.user.list')->with(['flash_level'=>'success','flash_message'=>'Xóa user thành công']);
     	}
     }
-	//Cập nhật user
+	//Đổi password
 	public function getEdit($id){
         $username = DB::table('users')->where('id',$id)->first();
         if ($username->level == 0 || Auth::user()->id != $id ){
@@ -71,14 +73,11 @@ class UserController extends Controller
     public function postEdit($id,Request $req){
 	$this->validate($req,
             [
-                'txtEmail'=>'email|unique:users,email',
                 'txtPass'=>'min:6|max:20',
                 'txtRePass'=>'same:txtPass',
                 'txtName'=>'min:3'
             ],
             [
-                'txtEmail.email'=>'Không đúng định dạng email',
-                'txtEmail.unique'=>'Email đã có người sử dụng',
                 'txtRePass.same'=>'Mật khẩu không giống nhau',
                 'txtPass.min'=>'Mật khẩu ít nhất 6 kí tự',
 				'txtName.min'=>'Tên hiện thị ít nhất 3 ký tự'
@@ -96,7 +95,23 @@ class UserController extends Controller
             $username->password = Hash::make($pass);
         }
         $username->tenhienthi = $req->txtName;
-        DB::table('users')->where('id',$id)->update(['password' => Hash::make($pass),'tenhienthi'=>$req->txtName]);
+        DB::table('users')->where('id',$id)->update(['password' => Hash::make($pass)]);
+        //return redirect()->route('admin.user.getEdit',$id)->with(['flash_level'=>'success','flash_message'=>'Cập nhật user thành công']);
+   		return	redirect()->route('admin.bill.showBill')->with(['flash_level'=>'success','flash_message'=>'Cập nhật user thành công']);
+   
+    }
+	//Cập nhật user
+	public function getEditUser($id){
+        $username = DB::table('users')->where('id',$id)->first();
+        if ( Auth::user()->id == $id ){
+            return redirect()->route('admin.user.list')->with(['flash_level'=>'danger','flash_message'=>'Bạn không thể cập nhật user này']);
+        }
+    	return view('admin.user.edit_user',compact('username'));
+    }
+
+    public function postEditUser($id,Request $req){
+	    $username->tenhienthi = $req->txtName;
+        DB::table('users')->where('id',$id)->update(['tenhienthi'=>$req->txtName,'level'=>$req->rdoLevel]);
         return redirect()->route('admin.user.list')->with(['flash_level'=>'success','flash_message'=>'Cập nhật user thành công']);
     }
 
--- resources/views/admin/user/edit_user.blade.php
+++ resources/views/admin/user/edit_user.blade.php
@@ -9,7 +9,7 @@ Cập nhật user
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">User
-                            <small>Cập user</small>
+                            <small>Cập user admin</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
@@ -21,24 +21,33 @@ Cập nhật user
                             {{--  Địa chỉ email  --}}
                             <div class="form-group">
                                 <label>Email (*)</label>
-                                <input class="form-control" name="txtEmail" value="{{$username->email}}"placeholder="Please Enter Email" disabled />
-                            </div>
-                            {{--  Mật khẩu cũ  --}}
-                            <div class="form-group">
-                                <label>Password (*)</label>
-                                <input type="password" class="form-control" name="txtPass"  placeholder="Please Enter Password" />
-                            </div>
-                            {{--  Mật khẩu mới --}}
-                            <div class="form-group">
-                                <label>RePassword (*)</label>
-                                <input type="password" class="form-control" name="txtRePass" placeholder="Please Enter RePassword" />
+                                <input class="form-control" name="txtEmail" value="{{$username->email}}"placeholder="Please Enter Email" disabled readonly/>
                             </div>
                             {{--  Tên hiện thị  --}}
                             <div class="form-group">
                                 <label>Tên hiện thị</label>
-                                <input type="text" class="form-control" name="txtName" value="{{$username->tenhienthi}}" placeholder="Please Enter Name" />
+                                <input type="text" class="form-control" name="txtName" value="{{$username->tenhienthi}}" placeholder="Please Enter Name" readonly/>
                             </div>
-                            
+                            {{--  Level  --}}
+                            <div class="form-group">
+                                <label>Level</label>
+								@if($username->level==1)
+									<label class="radio-inline">
+									<input name="rdoLevel" value="1" checked="" type="radio">Admin
+									</label>
+									<label class="radio-inline">
+										<input name="rdoLevel" value="2" type="radio">Member
+									</label>
+								@else
+									<label class="radio-inline">
+									<input name="rdoLevel" value="1"  type="radio">Admin
+									</label>
+									<label class="radio-inline">
+										<input name="rdoLevel" value="2" checked="" type="radio">Member
+									</label>
+
+								@endif
+                            </div>
                             <button type="submit" class="btn btn-default">User Edit</button>
                             <button type="reset" class="btn btn-default">Reset</button>
                         <form>
